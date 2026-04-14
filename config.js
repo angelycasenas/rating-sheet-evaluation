64c90ad549146184e6c911ee89d112726168cea7
angelycasenas <angelycasenas042@gmail.com>
2026-04-14 13:57:21 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+
+// 1. PALIHOG KOG BUTANG SA IMONG GOOGLE SCRIPT WEB APP URL ARI SALAMAT <3
+const GOOGLE_SCRIPT_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbwU52WzN8lErd1zI8XTsODrvdQFHkVLJa2YqYZBwtsm7A_Pz66gW0G8FwlrFkqPguz2/exec";
+
+// 2. PALIHOG PUD KOG BUTANG SA MGA LISTAHAN SA IMONG CASPTONE/RESEARCH TITLES ARI 
+const projectTitles = [
+  "AniCheck: An Android-Based Real-Time Freshness Detection of Meat and Chicken Using Colorimetric Analysis",
+  "Development of a Web-Based Sports Event Management System with Live Streaming Integration",
+  "Fever Detection with Embedded Automatic Medication System"
+];
